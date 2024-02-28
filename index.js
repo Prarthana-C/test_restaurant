f80350ffaf6aa6d6e7df2dc050443e77f18585e6
Prarthana-C <117766363+Prarthana-C@users.noreply.github.com>
2024-02-28 15:05:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/', function(req, res){
 });
 
 // PORT
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
